f43c3a4d2fad2b55b050f705dbb53d19fdf6e858
Nicholas_Miyasato <66659340+nixoletas@users.noreply.github.com>
2024-07-20 12:18:33 -0300

    Atualizar o server.js
    remover multer

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const multer = require('multer');
 const app = express();
 
 require('dotenv').config();
